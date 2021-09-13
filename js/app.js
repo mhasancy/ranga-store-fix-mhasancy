f61e90497a52735235906e180bc947aa7fdf1ed0
mhasacy <mhasancy@gmail.com>
2021-09-13 17:21:27 +0600

    code organized

--- js/app.js
+++ js/app.js
@@ -3,70 +3,52 @@ const loadProducts = () => {
   const url = `https://raw.githubusercontent.com/ProgrammingHero1/ranga-store-api/main/ranga-api.json`;
   fetch(url)
     .then((response) => response.json())
-    .then((data) => showProducts(data));
+    .then((data) => displayProducts(data));
 };
+//calling fetched products data
 loadProducts();
 
-// show all product in UI
-const showProducts = (products) => {
+// showing products in UI
+const displayProducts = (products) => {
+  // accessing products
   products.forEach((product) => {
     const div = document.createElement("div");
+    // shortenedProduct title with ternary
     const titleShort =
-      product.title.length >= 22
+      product.title.length > 22
         ? `${product.title.slice(0, 22)} ...`
         : product.title;
-    /*     const title =
-      product.title.length >= 25
-        ? `${product.title.slice(0, 28)} ...`
-        : product.title; */
+    //adding dynamic div
     div.classList.add("col");
-
     div.innerHTML = `<div class="card d-flex align-items-center justify-content-center text-center single-product">
     <img src=${product.image} class="product-image mt-4" alt="" >
     <div class="card-body">
-      <h3 class="card-title" ${
+      <h3 ${
         titleShort
           ? `title="${product.title}">${titleShort}</h3>`
           : `>${product.title}</h3>`
       }
       <p class="card-text">Category: ${product.category}</p>
       <h2 class="card-title">Price: $ ${product.price}</h2>
-      <span><i class="bi bi-star-fill text-warning"></i> <strong>${
-        product.rating.rate
-      }    </strong> out of 5 |<strong> ${
-      product.rating.count
-    } </strong>ratings</span>
+      <span>Reviews: <strong>${
+        product.rating.count
+      }</strong> • avg. ratings: <strong>${product.rating.rate}</strong></span>
       <div id="buttons-cart">
-      <button onclick="addToCart(${product.id},${
-      product.price
-    })" id="addToCart-btn"
+      <button onclick="addToCart(${product.price})" id="addToCart-btn"
         class="buy-now btn btn-dark">Add to Cart</button>
       <button id="details-btn" class="btn btn-warning text-white">Details</button>
       </div>
     </div>
 </div>`;
-    //   div.innerHTML = `<div class="single-product">
-    //     <div>
-    //   <img class="product-image" src=${product.image}></img>
-    //     </div>
-    //     <h3>${product.title}</h3>
-    //     <p>Category: ${product.category}</p>
-    //     <h2>Price: $ ${product.price}</h2>
-    //     <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-    //     <button id="details-btn" class="btn btn-danger">Details</button></div>
-    //     `;
+    //appending dynamic div
     document.getElementById("all-products").appendChild(div);
   });
 };
 
-// for (const product of allProducts) {
-//   // const image = product.image;
-
-// }
-
+//updating
 let count = 0;
-const addToCart = (id, price) => {
-  count = count + 1;
+const addToCart = (price) => {
+  count += 1;
   updatePrice("price", price);
 
   updateTaxAndCharge();
@@ -91,7 +73,6 @@ const updatePrice = (id, value) => {
 // set innerText function
 const setInnerText = (id, value) => {
   document.getElementById(id).innerText = value.toFixed(2);
-  // document.getElementById("delivery-charge").innerText = value;
 };
 
 // update delivery charge and total Tax
